a6c69c44d766ddf827dca1ddab17688c4278d85f
Celestine175 <mmekaebubechukwu123@gmail.com>
2024-09-12 23:13:22 +0100

    Update question2.java

--- question2.java
+++ question2.java
@@ -1,9 +1,11 @@
-import java.util.Arrays
-public class main{
-public static void main(string[] args{
-// initialize a two-dimensional array with rows of different lengths
-int[][] my numbers={{1,2,3,4}, {5,6,7}}:
-// print the second row of array
-system.out.printin(Arrays tostring(my number[0])]// output{1,2,3,4}
-      }
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize a two-dimensional array with rows of different lengths
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        
+        // Print the second row of the array
+        System.out.println(Arrays.toString(myNumbers[1])); // output: [5, 6, 7]
     }
+}
