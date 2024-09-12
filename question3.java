0e917e86fa6cac7e96049f79a7583f2b682f036c
Celestine175 <mmekaebubechukwu123@gmail.com>
2024-09-12 23:14:32 +0100

    Formatted question3 file

--- question3.java
+++ question3.java
@@ -1,8 +1,11 @@
-public class main{
-public static void main(string[] args){
-string[]names={Augustine, Chiemelie, Celestine, Samuel}:
-for(int;=0;i<names length;1++){
-system.out.printin(names[1]):
-       }
-     }
-   }
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize a two-dimensional array with rows of different lengths
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        
+        // Print the second row of the array
+        System.out.println(Arrays.toString(myNumbers[1])); // output: [5, 6, 7]
+    }
+}
