d71600d5987eeb9c705b090e6996d47760e030fa
Celestine175 <mmekaebubechukwu123@gmail.com>
2024-09-12 23:12:41 +0100

    Restructured question1 file

--- question1.java
+++ question1.java
@@ -1,7 +1,8 @@
-import java.util.Arrays
-public class main {
-public static void main(string[] args){
-string[] cars={“volvo”, “Bmw”, “Ford”, “Mazda”}
-system.out.printin(Arrays.toString(cars):// output
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(Arrays.toString(cars)); // output
     }
-   }
+}
